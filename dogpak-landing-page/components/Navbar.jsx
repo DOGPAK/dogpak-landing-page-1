6f460a61a1e9ff70c81e991d7daaae8b55ac8091
Lakshay <lakshayyadav2006@gmail.com>
2023-03-26 14:29:10 +0530

    made the navbar responsive.

--- dogpak-landing-page/components/Navbar.jsx
+++ dogpak-landing-page/components/Navbar.jsx
@@ -4,17 +4,42 @@ import Image from "next/image";
 import logo from "../static/logo.png";
 
 const Navbar = () => {
+
+    const showNavbar = () => {
+        const mobileNavbar = document.querySelector(`.${styles.mobileNavbar}`);
+        mobileNavbar.classList.toggle(styles.show);
+    }
+
     return (
         <nav className={styles.navbar}>
             <Image className={styles.logo} src={logo} alt={"hello"}/>
 
-            <div className={styles.navlinks}>
+            <div className={styles.navLinks}>
                 <p>HOME</p>
                 <p>WHY US</p>
                 <p>ABOUT</p>
             </div>
 
             <a className={styles.buyButton} href={"#"}>BUY NOW</a>
+
+            <div className={styles.burger} onClick={showNavbar}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"/>
+                </svg>
+            </div>
+
+            <div className={styles.mobileNavbar}>
+                <p>HOME</p>
+                <p>WHY US</p>
+                <p>ABOUT</p>
+            </div>
         </nav>
     );
 };
